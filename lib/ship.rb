32d3bf89ab91c02d3d5c25d2ec089495ee4ed65c
Archie Fielding <archiefielding@gmail.com>
2015-09-07 16:24:11 +0100

    basic ship status tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,26 @@
 class Ship
 
+def initialize
+	@floating = true
+end
+
+def floating?
+	@floating
+end
+
+def sunk?
+	@floating = false
+end
+
 def hit
 end
 
-
+def status
+	if floating?
+		"All fine"
+	else
+		"Sunk"
+	end
+end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,15 +1,36 @@
 require 'Ship'
 
 describe Ship do
+
+  it "should be floating when created" do
+  	expect(subject).to be_floating
+  end
+
   it 'can be hit' do
     expect(subject).to respond_to :hit
   end
 
-  xit "Ship can report status"
+  it "Ship can report status" do
+  	expect(subject).to respond_to :status
+  end
 
-  xit 'Ship can report hit'
+  it "can't be floating if sunk" do
+  	subject.sunk?
+  	expect(subject).to_not be_floating
+  end
+
+  # it 'Ship can respond with status "All fine"' do
+  # 	expect(subject.status).to eq("All fine") if floating?
+  # end
+
+  # it 'Ship can respond with status "Sunk"' do
+  # 	expect(subject.status).to eq("Sunk")
+  # end
+
+  it 'can report being hit' do
+  	
+  end
 
   xit "Ship sinks if each part is hit"
 
-  xit "Ship should be floating when created"
 end
